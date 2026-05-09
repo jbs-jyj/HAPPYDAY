8e408e134068ad07f93caac12297173b924be582
jbs-jyj <jbs36_1405@js.hs.kr>
2026-05-09 20:07:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,13 @@ async function searchHolidays() {
 
   result.innerHTML = "";
 
-  const serviceKey = "ad45fb2710a84c1d182b19ee083b656290d8d385860bd75c7c1ac35d83ad195c";
+const serviceKey = "ad45fb2710a84c1d182b19ee083b656290d8d385860bd75c7c1ac35d83ad195c";
 
-  const url =
-  "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo"
-  + "?serviceKey=" + serviceKey
+const url =
+  "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?serviceKey="
+  + serviceKey
   + "&solYear=" + year
   + "&solMonth=" + month;
-  try {
 
     const response = await fetch(url);
     const text = await response.text();
